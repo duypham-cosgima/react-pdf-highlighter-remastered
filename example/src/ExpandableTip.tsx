d5977d72103b64c37ddf742031b724d2a21bced2
Duy Pham <duy.pham@cosigma.io>
2025-12-15 17:20:01 +0700

    Fix missing property in example app

--- example/src/ExpandableTip.tsx
+++ example/src/ExpandableTip.tsx
@@ -6,6 +6,7 @@ import {
   usePdfHighlighterContext,
 } from "./react-pdf-highlighter-extended";
 import "./style/ExpandableTip.css";
+import { ghostHighlightStyle } from "../../src/lib/canvas";
 
 interface ExpandableTipProps {
   addHighlight: (highlight: GhostHighlight, comment: string) => void;
@@ -41,6 +42,7 @@ const ExpandableTip = ({ addHighlight }: ExpandableTipProps) => {
           onSubmit={(input) => {
             addHighlight(
               {
+                style: ghostHighlightStyle,
                 content: selectionRef.current!.content,
                 type: selectionRef.current!.type,
                 position: selectionRef.current!.position,
